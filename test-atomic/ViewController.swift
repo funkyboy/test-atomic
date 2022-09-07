af3949c9348f013fdd52387e22de73437a22e3bb
Cesare <cesarerocchi@gmail.com>
2022-09-07 11:14:26 +0200

    Initial Commit

---
+++ test-atomic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test-atomic
+//
+//  Created by Cesare Rocchi on 07/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
